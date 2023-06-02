d8ba7493991c1b2389cc9b96bd57c168e2566c19
mai-ogiso <little.bellflower@gmail.com>
2023-06-03 05:40:02 +0900

    remove comment out code

--- import-japanese-holidays.js
+++ import-japanese-holidays.js
@@ -15,36 +15,6 @@ const holidaysUrl = 'https://holidays-jp.github.io/api/v1/date.json';
 const now = new Date();
 const thisYear = now.getFullYear();
 
-// // Initialize i to count pages
-// let i = 0;
-
-// // Fetch the list of holidays from the API
-// (async () => {
-//   fetch(holidaysUrl)
-//     .then(response => response.json())
-//     .then(holidays => {
-//       // Loop through the list of holidays and add each one to my database
-//       for (const [key, value] of Object.entries(holidays)) {
-//         // Get the holiday data for this year and later years
-//         if (Number(key.slice(0, 4)) >= thisYear) {
-//           notion.pages.create({
-//             parent: { database_id: databaseId },
-//             properties: {
-//               Name: { title: [{ text: { content: `\u{1F1EF}\u{1F1F5} ${value}` } }] },
-//               Date: { date: { start: key } },
-//               // Tags: { select: { name: "祝日" } } // Add a "祝日" tag to each holiday
-//             }
-
-//           })
-//           i++;
-//         }
-//       }
-//     })
-//     .then(() => console.log(`Created ${i} pages!`))
-//     .catch(error => console.error(error));
-// })();
-
-
 // Define an async function to fetch the holidays and add them to the database
 async function fetchHolidays() {
   try {
